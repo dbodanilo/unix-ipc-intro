d5575e9cf17013e4922b12cb8cfc56b60a54192a
danilo <unifdaoliveira@fei.edu.br>
2021-10-03 22:06:25 -0300

    add Internet domain stream connection example

---
+++ streamwrite.c
@@ -0,0 +1,64 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+
+#define h_addr h_addr_list[0]
+
+#define DATA "Half a league, half a league . . ."
+
+/* 
+ * This program creates a socket and initiates a connection 
+ * with the socket given in the command line. 
+ * One message is sent over the connection and then the 
+ * socket is closed, ending the connection. 
+ * The form of the command line is:
+ * streamwrite hostname portnumber
+ */
+
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    perror("usage: streamwrite hostname portnumber");
+    exit(1);
+  }
+
+  int sock, length;
+  struct sockaddr_in server;
+  struct hostent *hp, *gethostbyname();
+ 
+  /* Create socket. */
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock < 0) {
+    perror("opening stream socket");
+    exit(1);
+  }
+
+  /* Connect socket using name specified by command line. */
+  server.sin_family = AF_INET;
+  hp = gethostbyname(argv[1]);
+  if (hp == 0) {
+    fprintf(stderr, "%s: unknown host", argv[1]);
+    exit(2);
+  }
+
+  bcopy(hp->h_addr, &server.sin_addr, hp->h_length);
+  server.sin_port = htons(atoi(argv[2]));
+
+  length = sizeof(server);
+  if (connect(sock, (struct sockaddr*)&server, length) < 0) {
+    perror("connecting stream socket");
+    exit(1);
+  }
+
+  if (write(sock, DATA, sizeof(DATA)) < 0) 
+    perror("writing on stream socket");
+
+  close(sock);
+
+  return 0;
+}
+
